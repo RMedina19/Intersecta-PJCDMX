6c88db24aabb9309f35ce113d6e6209d3e0724fb
RMedina19 <51415761+RMedina19@users.noreply.github.com>
2021-01-20 16:20:38 -0600

    Contabilización de observaciones

--- análisis/01_limpieza.R
+++ análisis/01_limpieza.R
@@ -3,7 +3,7 @@
 # Encargada:                    Regina Isabel Medina Rosales
 # Correo:                       rmedina@intersecta.org
 # Fecha de creación:            14 de enero de 2021
-# Última actualización:         14 de enero de 2021
+# Última actualización:         20 de enero de 2021
 #------------------------------------------------------------------------------#
 
 # 0. Configuración inicial -----------------------------------------------------
@@ -24,130 +24,153 @@ out <- "datos_limpios/"
 
 # Sobre delitos cometidos en la Ciudad de México entre mayo-2011 y sep-2020
 # Información de los asuntos ingresados por los diversos delitos
-df_asuntos_crudo        <- read_excel("datos_crudos/F227220_081220 INFOMEX.xlsx", 
+df_asuntos_crudo        <- read_excel(paste0(inp, "F227220_081220 INFOMEX.xlsx"), 
                                skip = 8, sheet = "ingresados")
 
 # Información de las personas involucradas como víctimas u ofendidas 
-df_personas_crudo       <- read_excel("datos_crudos/F227220_081220 INFOMEX.xlsx", 
+df_personas_crudo       <- read_excel(paste0(inp, "F227220_081220 INFOMEX.xlsx"), 
                                skip = 8, sheet = "victOfend")
 
 # Información de los asuntos con resolución del auto de plazo constitucional
-df_sitjurid_crudo       <- read_excel("datos_crudos/F227220_081220 INFOMEX.xlsx", 
+df_sitjurid_crudo       <- read_excel(paste0(inp, "F227220_081220 INFOMEX.xlsx"), 
                                skip = 8, sheet = "situaJurid")  
 
 # Información de soluciones alternas o terminaciones anticipadas
-df_alternas_crudo       <- read_excel("datos_crudos/F227220_081220 INFOMEX.xlsx", 
+df_alternas_crudo       <- read_excel(paste0(inp, "F227220_081220 INFOMEX.xlsx"), 
                                skip = 8, sheet = "solAlternasTermAnticip")
 
 # Información de la medida cautelar de prisión preventiva
-df_cautelares_crudo     <- read_excel("datos_crudos/F227220_081220 INFOMEX.xlsx", 
+df_cautelares_crudo     <- read_excel(paste0(inp, "F227220_081220 INFOMEX.xlsx"), 
                                skip = 8, sheet = "medCautelares")
 
 # Información de las sentencias emitidas en primera instancia 
-df_sentencias_crudo     <- read_excel("datos_crudos/F227220_081220 INFOMEX.xlsx", 
+df_sentencias_crudo     <- read_excel(paste0(inp, "F227220_081220 INFOMEX.xlsx"), 
                                skip = 7, sheet = "sentencias")
 
         
 # 2. Limpiar datos -------------------------------------------------------------
 # Renombrar variables 
 df_asuntos <- df_asuntos_crudo          %>% 
-        rename(materia         = "Materia", 
-                indice_exp  = "Indice para agrupación 1\r\n\r\n(Expediente / Carpeta)", 
-                indice_per     = "Indice para agrupación 2\r\n\r\n(Personas)", 
-                año_ingreso    = "Año ingreso", 
-                month_ingreso    = "month ingreso", 
-                sexo_indiciada = "Sexo de la persona indiciada", 
-                edad_indiciada = "Edad de la persona indiciada",
-                tipo_delictivo = "Tipo delictivo", 
-                desag_estad    = "Desagregado estadístico", 
-                consignación   = "Consignacion", 
-                comisión       = "Comisión", 
-                realización    = "Realización", 
-                alcaldia_ocurrencia       = "alcaldia_ocurrencia de ocurrencia")
+        rename(materia           = "Materia", 
+                id_exp           = "Indice para agrupación 1\r\n\r\n(Expediente / Carpeta)", 
+                id_per_agresora  = "Indice para agrupación 2\r\n\r\n(Personas)", 
+                year_ingreso     = "Año ingreso", 
+                month_ingreso    = "Mes ingreso", 
+                sexo_indiciada   = "Sexo de la persona indiciada", 
+                edad_indiciada   = "Edad de la persona indiciada",
+                tipo_delictivo   = "Tipo delictivo", 
+                desag_estad      = "Desagregado estadístico", 
+                consignación     = "Consignacion", 
+                comision         = "Comisión", 
+                realizacion      = "Realización", 
+                alcaldia_ocurr   = "Alcaldía de ocurrencia")
 
 df_personas <- df_personas_crudo        %>% 
-        rename(indice_exp   = "Indice para agrupación 1\r\n(Expediente / Carpeta)", 
-                indice_per     = "Indice para agrupación 3\r\n(Persona)", 
-                sexo_victima   = "Sexo de la persona involucrada como víctima u ofendida", 
-                edad_victima   = "Edad de la persona involucrada como víctima u ofendida", 
-                relación       = "Relación entre la persona involucrada como víctima u ofendida y la persona probable responsable de la comisión del delito")        
+        rename(id_exp           = "Indice para agrupación 1\r\n(Expediente / Carpeta)", 
+                id_per_ofendida = "Indice para agrupación 3\r\n(Persona)", 
+                sexo_victima    = "Sexo de la persona involucrada como víctima u ofendida", 
+                edad_victima    = "Edad de la persona involucrada como víctima u ofendida", 
+                relación        = "Relación entre la persona involucrada como víctima u ofendida y la persona probable responsable de la comisión del delito")
 
 
 df_sitjurid <- df_sitjurid_crudo        %>%
-        rename(materia         = "Materia", 
-                indice_exp  = "Indice para agrupación 1\r\n\r\n(Expediente / Carpeta)", 
-                indice_per     = "Indice para agrupación 2\r\n\r\n(Persona)", 
-                año_resolucion = "Año resolución",  
-                month_resolucion = "month resolución", 
-                sexo_procesada = "Sexo de la persona procesada", 
-                edad_procesada = "Edad de la persona procesada",
-                tipo_delictivo = "Tipo delictivo", 
-                desag_estad    = "Desagregado estadístico", 
-                consignación   = "Consignacion", 
-                comisión       = "Comisión", 
-                realización    = "Realización", 
-                alcaldia_ocurrencia       = "alcaldia_ocurrencia de ocurrencia", 
-                resolución     = "Resolución a la situación jurídica") 
+        rename(materia           = "Materia", 
+                id_exp           = "Indice para agrupación 1\r\n\r\n(Expediente / Carpeta)", 
+                id_per_agresora  = "Indice para agrupación 2\r\n\r\n(Persona)", 
+                year_resolucion  = "Año resolución",  
+                month_resolucion = "Mes resolución", 
+                sexo_procesada   = "Sexo de la persona procesada", 
+                edad_procesada   = "Edad de la persona procesada",
+                tipo_delictivo   = "Tipo delictivo", 
+                desag_estad      = "Desagregado estadístico", 
+                consignación     = "Consignacion", 
+                comision         = "Comisión", 
+                realizacion      = "Realización", 
+                alcaldia_ocurr   = "Alcaldía de ocurrencia", 
+                resolución       = "Resolución a la situación jurídica") 
 
 df_alternas <- df_alternas_crudo          %>% 
         rename(materia         = "Materia", 
-                indice_exp  = "Indice para agrupación 1\r\n\r\n(Expediente / Carpeta)", 
-                indice_per     = "Indice para agrupación 2\r\n\r\n(Persona)", 
+                id_exp  = "Indice para agrupación 1\r\n\r\n(Expediente / Carpeta)", 
+                id_per_agresora     = "Indice para agrupación 2\r\n\r\n(Persona)", 
                 year_audiencia  = "Año audiencia", 
-                month_audiencia  = "month audiencia", 
+                month_audiencia  = "Mes audiencia", 
                 sexo_indiciada = "Sexo de la persona indiciada", 
                 edad_indiciada = "Edad de la persona indiciada",
                 tipo_delictivo = "Tipo delictivo", 
                 desag_estad    = "Desagregado estadístico", 
                 consignación   = "Consignacion", 
-                comisión       = "Comisión", 
-                realización    = "Realización", 
-                alcaldia_ocurrencia       = "alcaldia_ocurrencia de ocurrencia", 
+                comision       = "Comisión", 
+                realizacion    = "Realización", 
+                alcaldia_ocurr = "Alcaldía de ocurrencia", 
                 tipo_solución  = "Tipo de solución alterna o terminación anticipada")
 
 df_cautelares <- df_cautelares_crudo %>% 
         rename(materia         = "Materia", 
-                indice_exp  = "Indice para agrupación 1\r\n\r\n(Expediente / Carpeta)", 
-                indice_per     = "Indice para agrupación 2\r\n\r\n(Persona)", 
-                año_audiancia  = "Año audiencia",  
-                month_audiencia  = "month audiencia", 
+                id_exp  = "Indice para agrupación 1\r\n\r\n(Expediente / Carpeta)", 
+                id_per_agresora     = "Indice para agrupación 2\r\n\r\n(Persona)", 
+                year_audiancia  = "Año audiencia",  
+                month_audiencia  = "Mes audiencia", 
                 sexo_vinculada = "Sexo de la persona vinculada", 
                 edad_vinculada = "Edad de la persona vinculada",
                 tipo_delictivo = "Tipo delictivo", 
                 desag_estad    = "Desagregado estadístico", 
                 consignación   = "Consignacion", 
-                comisión       = "Comisión", 
-                realización    = "Realización", 
-                alcaldia_ocurrencia       = "alcaldia_ocurrencia de ocurrencia", 
+                comision       = "Comisión", 
+                realizacion    = "Realización", 
+                alcaldia_ocurr = "Alcaldía de ocurrencia", 
                 tipo_medida    = "Tipo de medida cautelar") 
 
 df_sentencias <- df_sentencias_crudo %>% 
         rename(materia         = "Materia", 
-                indice_exp  = "Indice para agrupación 1\r\n\r\n(Expediente / Carpeta)", 
-                indice_per     = "Indice para agrupación 2\r\n\r\n(Persona)", 
-                año_sentencia  = "Año sentencia",  
-                month_sentencia  = "month sentencia", 
+                id_exp  = "Indice para agrupación 1\r\n\r\n(Expediente / Carpeta)", 
+                id_per_agresora     = "Indice para agrupación 2\r\n\r\n(Persona)", 
+                year_sentencia  = "Año sentencia",  
+                month_sentencia  = "Mes sentencia", 
                 sexo_sentenciada = "Sexo", 
                 edad_sentenciada = "Edad",
                 tipo_delictivo = "Tipo delictivo", 
                 desag_estad    = "Desagregado estadístico", 
                 consignación   = "Consignación", 
-                comisión       = "Comisión", 
-                realización    = "Realización", 
-                alcaldia_ocurrencia       = "alcaldia_ocurrencia de ocurrencia del delito", 
+                comision       = "Comisión", 
+                realizacion    = "Realización", 
+                alcaldia_ocurr = "Alcaldía de ocurrencia del delito", 
                 forma_proceso  = "Forma del proceso", 
                 tipo_sentencia = "Tipo de sentencia", 
-                años_sentencia = "Años de sentencia", 
-                monthes_sentencia = "monthes de sentencia", 
+                years_sentencia = "Años de sentencia", 
+                months_sentencia = "Meses de sentencia", 
                 días_sentencia = "Días de sentencia") 
         
+
+# 3. Explorar datos -------------------------------------------------------------
+# Identificar número de observaciones, expedientes únicos y personas únicas
+
+dim(df_asuntos)[1]                              # 301,836 observaciones (asuntos)
+length(unique(df_asuntos$id_exp))               # 219,447 expedientes únicos
+length(unique(df_asuntos$id_per_agresora))      # 255,793 agresores únicos
+
+dim(df_personas)[1]                             # 335,319 observaciones (víctimas)
+length(unique(df_personas$id_exp))              # 231,186 expedientes únicos
+length(unique(df_personas$id_per_ofendida))     # 252,738 víctimas únicas
+
+dim(df_sitjurid)[1]                             # 203,053 observaciones (asuntos con resolución de auto plazo)
+length(unique(df_sitjurid$id_exp))              # 152,686 expedientes únicos
+length(unique(df_sitjurid$id_per_agresora))     # 177,683 agresores únicos
+
+dim(df_alternas)[1]                             # 28,699 observaciones (soluciones alternas)
+length(unique(df_alternas$id_exp))              # 21,987 expedientes únicos
+length(unique(df_alternas$id_per_agresora))     # 25,735 agresores únicos
+
+dim(df_cautelares)[1]                           # 176,812 observaciones (medidas cautelares)
+length(unique(df_cautelares$id_exp))            # 48,286 expedientes únicos
+length(unique(df_cautelares$id_per_agresora))   # 63,260 agresores únicos
+
+dim(df_sentencias)[1]                           # 129,497 observaciones (sentencias por delito)
+length(unique(df_sentencias$id_exp))            # 97,177 expedientes únicos 
+length(unique(df_sentencias$id_per_agresora))   # 116,378 agresores únicos
+
 
 
 # Fin del código #
 
-# Preguntas: 
-# 1. ¿A qué se refiere persona indiciada? 
-# 2. ¿Son excluyentes los casos de medidas cautelares y de soluciones alternas
-# 3. ¿El identificador de persona es para quien denuncia? 
-# 4. En el registro de sentencia, ¿el sexo se refiere a la persona sentenciada?
         
---
+++ análisis/02_visualización.R
@@ -0,0 +1,70 @@
+#------------------------------------------------------------------------------#
+# Objetivo:                     Procesar datos de la PJCDMX
+# Encargadas:                   Estefanía Vela y Regina I. Medina
+# Correo:                       rmedina@intersecta.org
+# Fecha de creación:            30 de enero de 2021
+# Última actualización:         20 de enero de 2021
+#------------------------------------------------------------------------------#
+
+# 0. Configuración inicial -----------------------------------------------------
+
+# Librerías 
+require(pacman)
+
+p_load(scales, tidyverse, stringi, dplyr, plyr, foreign, readxl, janitor,
+        extrafont, beepr, extrafont, treemapify, ggmosaic, srvyr, ggrepel, 
+        lubridate, cowplot)
+
+# Limpiar espacio de trabajo 
+rm(list=ls())
+
+# Establecer directorios 
+inp <- "datos_limpios/"
+out <- "figuras/"
+
+# 1. Cargar datos --------------------------------------------------------------
+
+# 2. Configuración del tema para visualización ---------------------------------
+
+tema <- theme_linedraw() +
+        theme(text = element_text(family = "Helvetica", color = "grey35"),
+                plot.title = element_text(size = 20, 
+                                        face = "bold", 
+                                        margin = margin(10,4,5,4), 
+                                        family="Helvetica", 
+                                        color = "black"),
+                plot.subtitle = element_text(size = 18, 
+                                        color = "#666666", 
+                                        margin = margin(5, 5, 5, 5), 
+                                        family="Helvetica"),
+                plot.caption = element_text(hjust = 1, 
+                                        size = 14, 
+                                        family = "Helvetica"),
+                panel.grid = element_line(linetype = 2), 
+                legend.position = "none",
+                panel.grid.minor = element_blank(),
+                legend.title = element_text(size = 16, 
+                                        family="Helvetica"),
+                legend.text = element_text(size = 16, 
+                                        family="Helvetica"),
+                legend.title.align = 1,
+                axis.title = element_text(size = 16, 
+                                        hjust = .5, 
+                                        margin = margin(1,1,1,1), 
+                                        family="Helvetica"),
+                axis.text = element_text(size = 16, 
+                                        face = "bold", 
+                                        family="Helvetica", 
+                                        angle=0, 
+                                        hjust=.5),
+                strip.background = element_rect(fill="#525252"),
+                strip.text.x = element_text(size=16, 
+                                        family = "Helvetica"),
+                strip.text.y = element_text(size=16, 
+                                        family = "Helvetica"))
+
+fill_base        <-  c("#F178B1","#998FC7", "#FF8C00", "#663399", "#C2F970", 
+                        "#00979C", "#B1EDE8", "#FE5F55")
+fill_autoridades <-  c("#F178B1","#998FC7", "#FF8C00", "#663399", "#C2F970", 
+                        "#00979C", "#B1EDE8", "#FE5F55", "#C52233")
+fill_dos         <-  c("#F178B1","#998FC7")
